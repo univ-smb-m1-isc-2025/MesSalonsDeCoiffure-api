5e316e45e399a5b01f9a2a005aefdbc5920195cb
DupLeo <leodupraz.r@gmail.com>
2025-01-24 17:33:28 +0100

    Initial commit

---
+++ src/test/java/org/lepaul/hairlab/HairLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lepaul.hairlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
