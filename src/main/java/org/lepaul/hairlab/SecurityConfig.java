1b0b793cb832fe65eeab3d437109718f77996a33
Paul_Coulm <paulcoulm@gmail.com>
2025-04-01 17:38:59 +0200

    Ajout route /checkUser + erreurs Cors ok (normalement) + création image local de l'appli (voir ReadMe)

---
+++ src/main/java/org/lepaul/hairlab/SecurityConfig.java
@@ -0,0 +1,9 @@
+package org.lepaul.hairlab;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    // Cette classe est vide, mais elle désactive tout filtre de sécurité restant
+}
+
